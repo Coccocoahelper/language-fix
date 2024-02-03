794e14bd220a699875fb9692c14e2ccc0be39210
Coccocoa's Helper (not the other one) <157546848+Coccocoahelper@users.noreply.github.com>
2024-02-03 13:16:52 +0000

    Update and rename ExampleMixin.java to LanguageOptionsScreenMixin.java

---
+++ src/main/java/net/fabricmc/example/mixin/LanguageOptionsScreenMixin.java
@@ -0,0 +1,22 @@
+package net.fabricmc.example.mixin;
+
+import net.minecraft.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(
+	targets = {"net/minecraft/client/gui/screen/options/LanguageOptionsScreen$LanguageSelectionListWidget"}
+)
+public class LanguageOptionsScreenMixin {
+	@Redirect(
+		method = {"selectEntry"},
+		at = @At(
+		value = "INVOKE",
+		target = "Lnet/minecraft/client/MinecraftClient;stitchTextures()V"
+		)
+	)
+	private void replaceStitching(MinecraftClient client) {
+		client.getLanguageManager().reload(client.getResourceManager());
+	}
+}
