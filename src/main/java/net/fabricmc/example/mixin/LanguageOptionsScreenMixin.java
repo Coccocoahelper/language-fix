4c5c713ffa9680e459873b353e9089bd38deb85f
Coccocoa's Helper (not the other one) <157546848+Coccocoahelper@users.noreply.github.com>
2024-02-03 13:23:35 +0000

    Update LanguageOptionsScreenMixin.java

--- src/main/java/net/fabricmc/example/mixin/LanguageOptionsScreenMixin.java
+++ src/main/java/net/fabricmc/example/mixin/LanguageOptionsScreenMixin.java
@@ -1,6 +1,6 @@
 package net.fabricmc.example.mixin;
 
-import net.minecraft.MinecraftClient;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
